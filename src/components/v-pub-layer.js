0ba198f6294b41da8da0cd0ff64421106a804bb7
Mobee <wangchen9321@gmail.com>
2024-08-16 10:44:57 +0800

    丰富了publayer的样式

--- src/components/v-pub-layer.js
+++ src/components/v-pub-layer.js
@@ -1,24 +1,85 @@
 import { useMapLife } from './common'
 import { VectorTileLayer } from '@maptalks/vt'
-import { watch, onBeforeUnmount } from 'vue'
+import { watch, onBeforeUnmount, computed, ref } from 'vue'
 import { getStyle } from '../services/style'
 
 export default {
   name: 'VPubLayer',
   props: {
-    id: String
+    id: String,
+    filter: {
+      type: Array,
+      default: () => []
+    }
   },
   setup(props, context) {
     const { onMapMounted, ee } = useMapLife()
     let tileLayer
+
+    function string2array(str) {
+      if (str) {
+        return JSON.parse(str)
+      } else {
+        return []
+      }
+    }
+
+    function prepareSymbol(originalSymbol) {
+      const { markerFileType, markerFile, markerFileProp, markerFilePropList, ...rest } =
+        originalSymbol
+      if (markerFileType === 'rule') {
+        return {
+          markerFile: {
+            type: 'categorical',
+            property: markerFileProp,
+            stops: string2array(markerFilePropList),
+            default: markerFile
+          },
+          ...rest
+        }
+      } else {
+        return { markerFile, ...rest }
+      }
+    }
+
+    const layerOptions = computed(() => {
+      const {
+        style: { symbol, ...restStyle },
+        ...restData
+      } = layerOriginOptions.value
+      const nextOptions = {
+        features: true,
+        ...restData,
+        style: {
+          ...restStyle,
+          symbol: prepareSymbol(symbol)
+        }
+      }
+      if (props.filter.length > 0) {
+        nextOptions.style.filter = props.filter
+      }
+      return nextOptions
+    })
+
+    const layerOriginOptions = ref({})
+
     onMapMounted((map) => {
       getStyle(props.id).then((res) => {
         if (res.code === 200) {
-          tileLayer = new VectorTileLayer(props.id, { features: true, schema: true, ...res.data })
+          layerOriginOptions.value = res.data
+          tileLayer = new VectorTileLayer(props.id, layerOptions.value)
           tileLayer.addTo(map)
         }
       })
     })
+    watch(
+      () => props.filter,
+      () => {
+        if (tileLayer) {
+          tileLayer.setOptions(layerOptions.value)
+        }
+      }
+    )
     onBeforeUnmount(() => {
       tileLayer.remove()
     })
