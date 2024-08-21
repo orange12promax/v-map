09549a1f7edce8424d29eeefe25cd34e1794037f
Mobee <wangchen9321@gmail.com>
2024-08-21 16:02:08 +0800

    增加了地图中心的设置功能

--- src/components/VMap.vue
+++ src/components/VMap.vue
@@ -6,10 +6,10 @@
 </template>
 
 <script setup>
-import { onMounted, provide, ref, computed } from 'vue'
+import { onMounted, provide, ref, computed, watch } from 'vue'
 import { Map } from './maptalks'
 import 'maptalks/dist/maptalks.css'
-import { mapName, mapServer, mapEvent } from './config'
+import { mapName, mapServer, mapEvent, mapMethods } from './config'
 import EventEmitter from 'eventemitter3'
 
 const props = defineProps({
@@ -17,25 +17,50 @@ const props = defineProps({
   center: Array,
   zoom: Number
 })
+const emits = defineEmits(['update:center', 'update:zoom', 'moveend', 'click'])
 
 const mapRef = ref()
 const ee = new EventEmitter()
 const mapServerUrl = computed(() => props.server)
 const ready = ref(false)
 
+let map
+
+function addLayer(layer) {
+  return map?.addLayer(layer)
+}
+
 onMounted(() => {
-  const map = new Map('homemap', {
+  map = new Map('homemap', {
     center: props.center,
     zoom: props.zoom
   })
   map.on('click', (e) => {
-    ee.emit('click', e.coordinate)
+    const { coordinate } = e
+    ee.emit('click', coordinate)
+    emits('click', {
+      coordinate: [coordinate.x, coordinate.y]
+    })
+  })
+  map.on('moveend', (e) => {
+    const { coordinate } = e
+    emits('update:center', [coordinate.x, coordinate.y])
   })
   mapRef.value = map
   ready.value = true
 })
 
+watch(
+  () => props.center,
+  (nv) => {
+    map?.setCenter(nv)
+  }
+)
+
 provide(mapName, mapRef)
 provide(mapEvent, ee)
 provide(mapServer, mapServerUrl)
+provide(mapMethods, {
+  addLayer
+})
 </script>
--- src/components/config.js
+++ src/components/config.js
@@ -1,3 +1,4 @@
 export const mapName = Symbol('map-injection')
 export const mapEvent = Symbol('map-event')
 export const mapServer = Symbol('map-server')
+export const mapMethods = Symbol('map-methods')
