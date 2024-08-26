2c731c5ca2d8f3a2c67918b4fdbc792cf7352938
Mobee <wangchen9321@gmail.com>
2024-08-26 17:45:53 +0800

    发布新版本

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 export { default as VMap } from '@/components/maptalks/v-map.vue'
 export { default as VPubLayer } from './pub-layer/index.jsx'
 export { default as VTiandituLayer } from './tianditu/index.js'
+export { default as VVectorLayer } from '@/components/maptalks/v-vector-layer.vue'
 export { default as VMarker } from '@/components/maptalks/geometry/v-marker.js'
 export { default as VLine } from '@/components/maptalks/geometry/v-line.js'
