ad5cd2d578f5f1f4739694629240878fe88d877d
Mobee <wangchen9321@gmail.com>
2024-08-30 14:21:33 +0800

    增加多边形

---
+++ src/components/maptalks/geometry/v-polygon.js
@@ -0,0 +1,45 @@
+import { Polygon } from '@/components/maptalks/module.js'
+import { watch, inject, onBeforeUnmount, onMounted } from 'vue'
+
+export default {
+  name: 'VPolygon',
+  props: {
+    id: String,
+    coordinates: Array,
+    options: Object
+  },
+  setup(props, context) {
+    const { addGeometry } = inject('parentVectorLayer')
+    let geo
+    onMounted(() => {
+      geo = new Polygon(props.coordinates, { id: props.id, ...props.options })
+      addGeometry(geo)
+      geo.on('click', (e) => {
+        context.emit('click', e)
+      })
+    })
+    onBeforeUnmount(() => {
+      if (geo) {
+        geo.remove()
+      }
+    })
+
+    watch(
+      () => props.coordinates,
+      () => {
+        if (geo) {
+          geo.setCoordinates(props.coordinates)
+        }
+      }
+    )
+    watch(
+      () => props.options,
+      () => {
+        if (geo) {
+          geo.setOptions(props.options)
+        }
+      }
+    )
+    return () => null
+  }
+}
--- src/components/maptalks/module.js
+++ src/components/maptalks/module.js
@@ -5,5 +5,6 @@ export const Map = maptalks.Map
 export const TileLayer = maptalks.TileLayer
 export const Marker = maptalks.Marker
 export const Line = maptalks.LineString
+export const Polygon = maptalks.Polygon
 export const VectorLayer = maptalks.VectorLayer
 export const GeoJSON = maptalks.GeoJSON
